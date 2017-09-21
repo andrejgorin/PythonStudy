c23bd6a1416d35a94f5dfb32af9252dc36de2194
andrejgorin <31644587+andrejgorin@users.noreply.github.com>
2017-09-21 08:22:45 +0300

    Update ns.py

--- ns.py
+++ ns.py
@@ -5,7 +5,7 @@ import getpass
 import time
 
 def tmux(command):
-   subprocess.call('tmux %s' % command, shell=True)
+    subprocess.call('tmux %s' % command, shell=True)
 
 log = raw_input("login: ")
 password = getpass.getpass()
@@ -15,27 +15,25 @@ i = 1
 
 with open ("ip.txt", "r") as IP:
 
-   for line in IP:
-       output_list = "string"
-       tmux("new-window -t %d" % i)
-       tmux('select-window -t %d' % i)
-       tmux('rename-window "%s"' % line)
-       tmux('send-keys "telnet %s"' % line)
-       #time.sleep(1)
+    for line in IP:
+        output_list = "string"
+        tmux("new-window -t %d" % i)
+        tmux('select-window -t %d' % i)
+        tmux('rename-window "%s"' % line)
+        tmux('send-keys "telnet %s"' % line)
+		
+        while "Username:" not in output_list:
+            proc = subprocess.Popen('tmux capture-pane -p -t ns:%d' % i, stdout=subprocess.PIPE, shell=True)
+            output = proc.stdout.read()
+            output_list = output.split()
+        tmux('send-keys "%s" "Enter"' % log)
 
-       while "Username:" not in output_list:
-           proc = subprocess.Popen('tmux capture-pane -p -t ns:%d' % i, stdout=subprocess.PIPE, shell=True)
-           output = proc.stdout.read()
-           output_list = output.split()
-       time.sleep(1)
-       tmux('send-keys "%s" "Enter"' % log)
+        while "Password:" not in output_list:
+            proc = subprocess.Popen('tmux capture-pane -p -t ns:%d' % i, stdout=subprocess.PIPE, shell=True)
+            output = proc.stdout.read()
+            output_list = output.split()
 
-       while "Password:" not in output_list:
-           proc = subprocess.Popen('tmux capture-pane -p -t ns:%d' % i, stdout=subprocess.PIPE, shell=True)
-           output = proc.stdout.read()
-           output_list = output.split()
-
-       #time.sleep(1)
-       tmux('send-keys "%s" "Enter"' % password)
-       i +=1
+        time.sleep(1)
+        tmux('send-keys "%s" "Enter"' % password)
+        i +=1
 tmux("attach")
