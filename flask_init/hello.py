67e84ecfecb72f226f87755c6cfc50fe05759fdd
andrejgorin <andrejgorin@gmail.com>
2017-10-27 12:41:51 +0000

    commit

--- flask_init/hello.py
+++ flask_init/hello.py
@@ -1,13 +1,32 @@
 
 import os
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return 'index page'
+
+@app.route('/url_for')
+def url_for_demo():
+    return url_for('show_user_profile', username='richard')
+    
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'User {}'.format(username)
+    
+@app.route('/hello')
 def hello_world():
-    return 'Hello, world!'
+    #import pdb; pdb.set_trace() #from cli press 'n' to next or can execute commands, f.e. print(i)
+    i = 1
+    return 'You visited ' + str(i) + ' times'
+
+@app.route('/post/<int:post_id>') #define that post_id must be integer
+def show_post(post_id):
+    return 'Post {}'.format(post_id)
     
 if __name__ == '__main__':
     host = '0.0.0.0'
     port = 55000
+    app.debug = True
     app.run(host=host, port=port)
